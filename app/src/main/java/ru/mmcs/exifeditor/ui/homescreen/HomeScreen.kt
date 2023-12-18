2b67ffa00c94201369d5daf662893530d2112b80
Andrew Nikolaev <Andrew.Vaimon@yandex.ru>
2023-12-18 16:12:26 +0300

    Add image selection

--- app/src/main/java/ru/mmcs/exifeditor/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/ru/mmcs/exifeditor/ui/homescreen/HomeScreen.kt
@@ -1,10 +1,24 @@
 package ru.mmcs.exifeditor.ui.homescreen
 
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -15,11 +29,25 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.createBitmap
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import ru.mmcs.exifeditor.R
 import ru.mmcs.exifeditor.ViewModelProvider
 import ru.mmcs.exifeditor.navigation.NavigationDestination
@@ -39,6 +67,15 @@ fun HomeScreen(
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    val imgProviderLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.PickVisualMedia()) { target ->
+            target?.let {
+                viewModel.onImageChosen(it)
+            }
+        }
+
+    val uiState: HomeViewModel.UiState by viewModel.uiState
+
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
@@ -46,10 +83,33 @@ fun HomeScreen(
                 title = { Text(stringResource(HomeDestination.titleResourceId)) },
                 modifier = modifier,
                 scrollBehavior = scrollBehavior,
+                actions = {
+                    IconButton(onClick = {
+                        imgProviderLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                    }) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.upload),
+                            contentDescription = stringResource(R.string.upload)
+                        )
+                    }
+                }
             )
         },
+        floatingActionButton = {
+            if (uiState.imgSource != null) {
+                FloatingActionButton(onClick = {
+                    TODO()
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Edit,
+                        contentDescription = stringResource(R.string.edit)
+                    )
+                }
+            }
+        }
     ) { innerPadding ->
         HomeBody(
+            uiState.imgSource,
             modifier = Modifier.padding(innerPadding)
         )
     }
@@ -57,7 +117,49 @@ fun HomeScreen(
 
 @Composable
 fun HomeBody(
-    modifier: Modifier = Modifier
+    imgSource: Uri?,
+    modifier: Modifier = Modifier,
 ) {
+    if (imgSource == null) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "Choose an image to edit its data",
+                fontSize = 20.0.sp
+            )
+        }
+    } else {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.fillMaxSize()
+        ) {
+            AsyncImage(
+                model = imgSource,
+                contentDescription = stringResource(R.string.image),
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f, false)
+                    .background(color = Color.LightGray, RectangleShape)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f, true)
+                    .background(color = Color.Cyan, RectangleShape)
+            ) {
 
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewBody() {
+    HomeBody(imgSource = Uri.EMPTY)
 }
--- app/src/main/java/ru/mmcs/exifeditor/ui/homescreen/HomeViewModel.kt
+++ app/src/main/java/ru/mmcs/exifeditor/ui/homescreen/HomeViewModel.kt
@@ -1,6 +1,17 @@
 package ru.mmcs.exifeditor.ui.homescreen
 
+import android.net.Uri
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel(){
+class HomeViewModel : ViewModel() {
+    val uiState = mutableStateOf(UiState())
+
+    fun onImageChosen(uri: Uri) {
+        uiState.value = uiState.value.copy(imgSource = uri)
+    }
+
+    data class UiState(
+        val imgSource: Uri? = null
+    )
 }
